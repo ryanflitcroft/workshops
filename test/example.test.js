3dcd3a1714ac8cd18aad5092771a4f6505687042
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-12 17:51:35 -0800

    test

--- test/example.test.js
+++ test/example.test.js
@@ -1,19 +1,19 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-// import { renderParticipant } from '../render-utils.js';
+import { renderParticipant } from '../render-utils.js';
 
-// const test = QUnit.test;
+const { skip } = QUnit;
 
-// skip('renderParticipant should take in an object and return a span element with a class equal to participant, and textContent of the value of the name property', (expect) => {
+skip('renderParticipant should take in an object and return a span element with a class equal to participant, and textContent of the value of the name property', (expect) => {
     //Arrange 
     // Set up your arguments and expectations
-    // const expected = '<span class="participant">Ryan</span>';
+    const expected = '<span class="participant">Ryan</span>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    // const actual = renderParticipant({ name: 'Ryan' });
+    const actual = renderParticipant({ name: 'Ryan' });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-//     expect.equal(actual.outerHTML, expected, 'this test proves that the function returns a span with a class of participant and textContent of Ryan');
-// });
+    expect.equal(actual.outerHTML, expected, 'this test proves that the function returns a span with a class of participant and textContent of Ryan');
+});
